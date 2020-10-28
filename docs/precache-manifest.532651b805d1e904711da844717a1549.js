a9991ca299cd10982ff9587816ad5a8fb7284d88
GitHub Action <action@github.com>
2020-10-28 23:13:01 +0000

    Update build

---
+++ docs/precache-manifest.532651b805d1e904711da844717a1549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4587230cb875edc00d",
+    "url": "/css/app.b229284b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "05b70a8e71b93242b3a085caa99fcc7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bb78ead9fe1bff82a59",
+    "url": "/js/about.e8224201.js"
+  },
+  {
+    "revision": "7a4587230cb875edc00d",
+    "url": "/js/app.49805875.js"
+  },
+  {
+    "revision": "7c53bca4e46098a8ca60",
+    "url": "/js/chunk-vendors.553ea1dc.js"
+  },
+  {
+    "revision": "8e4efa0e2daed70ad44979c13ae29f5a",
+    "url": "/manifest.json"
+  }
+]);
