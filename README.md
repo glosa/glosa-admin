05326d13f4c2eb4057610017ba2ee31d5c54fc10
Andros Fenollosa <andros@fenollosa.email>
2020-10-28 23:53:49 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # glosa-admin
 
-## Project setup
+PWA READY glosa-admin.andros.dev
+
+## Development
+
 ```
 npm install
 ```
@@ -19,6 +22,3 @@ npm run build
 ```
 npm run lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ dist/precache-manifest.24c05965dc9d291a85f2bc6394e24475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3656ffafa709dfbfcd",
+    "url": "/css/app.b229284b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2d18d53209f0e56114fcafb41acc9e57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c88fb6a13a00c76077b",
+    "url": "/js/about.bcf02665.js"
+  },
+  {
+    "revision": "bc3656ffafa709dfbfcd",
+    "url": "/js/app.f34dc408.js"
+  },
+  {
+    "revision": "7c53bca4e46098a8ca60",
+    "url": "/js/chunk-vendors.553ea1dc.js"
+  },
+  {
+    "revision": "8e4efa0e2daed70ad44979c13ae29f5a",
+    "url": "/manifest.json"
+  }
+]);
